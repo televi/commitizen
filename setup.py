5b49ecc44a4b0b086f20f82d4256d30808158fef
Todd Levi <todd.levi@nokia.com>
2019-01-28 13:45:32 -0600

    chore: convert for ir use
    Simplified setup.py for IR use.  Added scratchpad/ per other IR projects to .gitignore

--- setup.py
+++ setup.py
@@ -1,28 +1,21 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Note: To use the 'upload' functionality of this file, you must:
-#   $ pip install twine
 
 import io
 import os
-import sys
-from shutil import rmtree
 
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = "commitizen"
-DESCRIPTION = "Python commitizen client tool."
-URL = "https://github.com/woile/commitizen"
-EMAIL = "santiwilly@gmail.com"
-AUTHOR = "Santiago Fraire"
+NAME = "committer"
+DESCRIPTION = "Commit to git using Convnetional Commits standard."
+URL = "https://git.cic-nfv.com/Solutions/commiter"
+EMAIL = "todd.levi@nokia.com"
+AUTHOR = "Santiago Fraire, Todd Levi"
 REQUIRED = [
     "delegator.py",
     "PyInquirer",
     "future",
-    "prompt-toolkit==1.0.15",
-    'pathlib2; python_version < "3.4"',
+    "prompt-toolkit>=1.0.15",
     'configparser; python_version < "3.5"',
 ]
 
@@ -34,40 +27,6 @@ with io.open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
 about = {}
 with open(os.path.join(here, NAME, "__version__.py")) as f:
     exec(f.read(), about)
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = "Build and publish the package."
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print("\033[1m{0}\033[0m".format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status("Removing previous builds…")
-            rmtree(os.path.join(here, "dist"))
-        except OSError:
-            pass
-
-        self.status("Building Source and Wheel (universal) distribution…")
-        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
-
-        self.status("Uploading the package to PyPi via Twine…")
-        os.system("twine upload dist/*")
-
-        sys.exit()
-
 
 setup(
     name=NAME,
@@ -87,14 +46,9 @@ setup(
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
     ],
-    # $ setup.py publish support.
-    cmdclass={"upload": UploadCommand},
 )
